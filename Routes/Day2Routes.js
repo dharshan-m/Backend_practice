92b9d6794d601a13234ddaa173dd907a7140cae2
dharshan-m <dharshanmohan2704@gmail.com.com>
2025-06-27 06:38:26 +0530

    completed the day4 task making crud operation

--- Routes/Day2Routes.js
+++ Routes/Day2Routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {createUser} = require('../controller/day2Controller');
+const {createUser, getUser, updateUser, deleteUser} = require('../controller/day2Controller');
 
 router.post('/create', createUser);
-
+router.get('/get', getUser);
+router.put('/update/:id', updateUser);
+router.delete('/delete/:id', deleteUser);
 
 module.exports = router;
--- controller/day2Controller.js
+++ controller/day2Controller.js
@@ -11,10 +11,55 @@ const createUser = async(req,res)=>{
         res.status(201).json({message:"User Created Successfully", user:newUser});
 
     }catch(error){
-        res.status(500).json({message:"Internal Server Error", error:error.message});
+        res.status(500).json({message:error.message});
+    }
+}
+
+const getUser = async(req,res)=>{
+    try{
+        const {Name} = req.body;
+        const users = await day2Model.findOne({Name});
+        if(!users) return res.status(404).json({message:"No user fond"});
+
+        res.status(200).json({message:"User Found", user:users});
+
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+const updateUser = async(req,res)=>{
+    try{
+        const {id} = req.params
+        const {Name, Email, Password, Age} = req.body;
+
+        const user = await day2Model.findOne({Name});
+        if(!Name) return res.status(404).json({message:"User Not Found"});
+
+        const updatedUser = await day2Model.findByIdAndUpdate(id, {Name, Email, Password, Age}, {new:true});
+        res.status(200).json({message:"User Updated Successfully", user:updatedUser});
+    }
+    catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+const deleteUser = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const user = await day2Model.findOne({id});
+        if(!user) return res.status(404).json({message:"User Not Found"});
+
+        const deletedUser = await day2Model. findOneByIdAndDelete({id});
+        res.status(200).json({message:"User Deleted Successfully", user:deletedUser})
+    }catch(error){
+        res.status(500).json({message:error.message});
     }
 }
 
 module.exports = {
-    createUser
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser
 }
